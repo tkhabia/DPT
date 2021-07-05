9a8950c52131425a2e723adc0d9cca6b87c29713
tkhabia <tanmay.khabia@students.iiit.ac.in>
2021-07-05 15:34:32 +0530

    updated io.py file

--- util/io.py
+++ util/io.py
@@ -204,7 +204,7 @@ def write_segm_img(path, image, labels, palette="detail", alpha=0.5):
         labels (array): labeling of the image
     """
     relevant_labels = [1 ,16,20,13]
-    replaceable = {11:16, 24:20 ,31:20 ,34:16 , 46:16,54:1,60:1, 111:20 }
+    replaceable = {11:16, 24:20 ,31:20 ,34:16 , 46:16,54:1,60:1, 111:20,43:1 , 2:1 }
     for i in range(len(labels)):
         for j in range(len(labels[0])):
             if labels[i,  j ] not in relevant_labels:
